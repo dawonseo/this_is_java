b9853f315d02f7c305ac02193181c31a9c823edc
dawonseo <oloielo@naver.com>
2024-04-01 19:30:02 +0900

    12챕터-11까지

--- src/ch12/sec03/exam05/Member.java
+++ src/ch12/sec03/exam05/Member.java
@@ -2,11 +2,9 @@ package ch12.sec03.exam05;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Member {
     private String id;
---
+++ src/ch12/sec03/exma02/Student.java
@@ -0,0 +1,5 @@
+package ch12.sec03.exma02;
+
+public class Student {
+
+}
